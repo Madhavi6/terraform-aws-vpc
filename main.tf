53b905442820ad738aceec45aae71cfe7a57d04e
Madhavi <patel.madhavi@gmail.com>
2026-03-25 19:36:51 +0200

    added private subnet

--- main.tf
+++ main.tf
@@ -26,6 +26,19 @@ resource "aws_subnet" "public" {
   )
 }
 
+resource "aws_subnet" "private" {
+  count = length(var.private_subnet_cidrs)
+
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = local.az_names[count.index]
+  
+  tags = merge(local.common_tags, {
+    "Name" = "${var.project}-${var.environment}-private-${local.az_names[count.index]}"
+  }, var.private_subnet_tags
+  )
+}
+
 resource "aws_subnet" "database" {
   count = length(var.database_subnet_cidrs)
   vpc_id            = aws_vpc.main.id
